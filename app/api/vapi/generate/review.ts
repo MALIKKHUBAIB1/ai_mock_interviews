28d03d93df04d4ef6804e105a28e3d7994246655
MALIKKHUBAIB1 <khubaib9221@gmail.com>
2025-04-22 16:30:13 +0530

    api key error

--- app/api/vapi/generate/review.ts
+++ app/api/vapi/generate/review.ts
@@ -1,6 +1,9 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI_API_KEY);
+const genAI = new GoogleGenerativeAI(
+  process.env.GOOGLE_GENERATIVE_AI_API_KEY ||
+    "AIzaSyBDH28QBdtwag99q_sxINkGr3R1HHSUamo"
+);
 const model = genAI.getGenerativeModel({
   model: "gemini-2.0-flash",
   systemInstruction: `
